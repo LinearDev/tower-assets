162f9b9b67d85316966f385e8660bf17a7283a39
H1kiray <vld.orishich@gmail.com>
2022-06-15 22:51:03 +0300

    update script

--- Scripts/MainMenu.cs
+++ Scripts/MainMenu.cs
@@ -7,7 +7,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        //hello vladik
+        Console.Write("Hello World");
     }
 
     // Update is called once per frame
