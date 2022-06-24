fac0fb88c5f5c0ee5c6d32368c99902870d09bc1
H1kiray <vld.orishich@gmail.com>
2022-06-24 21:29:13 +0300

    Update script

--- Scripts/Tower.cs
+++ Scripts/Tower.cs
@@ -8,25 +8,25 @@ public class Tower : MonoBehaviour
     public int hp = 1000;
     public float mana = 100;
     public float lvl = 1;
-    Rigidbody rb;
     void Start()
     {
-        rb = GetComponent<Rigidbody>(); // Запуск функции, вызов тела или типо того
+       
     }
 
     void Update()
     {
 
     }
-    void TakeDamage(Collision shit) // Должно отвечать за снижение хп при столкновении, но чёт нихуя не работает
-    {
-        if (shit.gameObject.tag == "Enemy")
-        {
-            hp -= 100;
-        }
-    }
     void OnGUI() // Для вывода менюшки с хп
     {
         GUI.Box(new Rect(0, 0, 100, 30), "hp = " + hp); // 0 и 0 это корды, дальше размер 100х30, и дальше hp = [1000]
+    }
+
+    void OnCollisionEnter(Collision shit) // Должно отвечать за снижение хп при столкновении, но чёт нихуя не работает
+    {
+        if (shit.gameObject.name == "Enemy")
+        {
+            hp -= 100;
+        }
     }
 }
